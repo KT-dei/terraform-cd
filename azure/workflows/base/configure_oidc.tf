980d7de3a79bec5cc7537914ef8730cfa9f2c76d
ivanhidalgo22 <ivanhidalgo22@gmail.com>
2025-04-07 17:55:32 -0500

    formatted variables

--- azure/workflows/base/configure_oidc.tf
+++ azure/workflows/base/configure_oidc.tf
@@ -5,15 +5,15 @@
 module "github-oidc-uami" {
   source = "../../modules/github-oidc-uami"
 
-  name                = var.oidc_name
-  location            = var.location
-  resource_group_name = local.rg_k8s
-  github_repo         = var.github_repo
-  branch              = var.branch
-  scope               = "/subscriptions/${var.azure_subscription_id_global}"
-  role_definition_name = "Managed Identity Operator"
-  subject             = "repo:${var.github_repo}:ref:refs/heads/${var.branch}"
+  name                       = var.oidc_name
+  location                   = var.location
+  resource_group_name        = local.rg_k8s
+  github_repo                = var.github_repo
+  branch                     = var.branch
+  scope                      = "/subscriptions/${var.azure_subscription_id_global}"
+  role_definition_name       = "Managed Identity Operator"
+  subject                    = "repo:${var.github_repo}:ref:refs/heads/${var.branch}"
   storage_account_blob_scope = "/subscriptions/${var.azure_subscription_id_global}/resourceGroups/${var.tfstate_rg}/providers/Microsoft.Storage/storageAccounts/${var.tfstate_storage_account_name}"
-  resource_group_scope = "/subscriptions/${var.azure_subscription_id_global}/resourceGroups/${var.tfstate_rg}"
-  
+  resource_group_scope       = "/subscriptions/${var.azure_subscription_id_global}/resourceGroups/${var.tfstate_rg}"
+
 }
