2f0793d33717cdcb0b25ca06ceb509690e6b4882
ivanhidalgo22 <ivanhidalgo22@gmail.com>
2025-04-07 18:49:48 -0500

    formatted variables

--- azure/workflows/base/backend.tf
+++ azure/workflows/base/backend.tf
@@ -1,10 +1,10 @@
-terraform {
-  backend "azurerm" {
-    resource_group_name  = "terraform-rg"
-    storage_account_name = "tfstate26035"
-    container_name       = "tfstate"
-    key                  = "terraform.tfstate"
-    subscription_id      = "fd95eef5-ba7d-4f3a-bdc0-1d012014be02"
+#terraform {
+  #backend "azurerm" {
+    #resource_group_name  = "terraform-rg"
+    #storage_account_name = "tfstate26035"
+    #container_name       = "tfstate"
+    #key                  = "terraform.tfstate"
+    #subscription_id      = "fd95eef5-ba7d-4f3a-bdc0-1d012014be02"
 
-  }
-}
+  #}
+#}
